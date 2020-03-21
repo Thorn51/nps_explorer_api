8b9155720ef77eb1b3ef6677bb21213785807092
Matt Thornberry <mtberry16@hotmail.com>
2020-03-21 13:51:51 -0600

    Add users array

---
+++ test/fixtures.js
@@ -0,0 +1,44 @@
+function makeUsersArray() {
+  return [
+    {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@devevlopmenttesting.com",
+      password: "notRealForDev!1",
+      nickname: null,
+      home_state: null,
+      date_created: "2020-12-21T07:00:00.000Z"
+    },
+    {
+      id: 2,
+      first_name: "Luke",
+      last_name: "Doe",
+      email: "ldoe@devevlopmenttesting.com",
+      password: "notRealForDev2!",
+      nickname: "nps_friend",
+      home_state: "CO",
+      date_created: "2020-12-21T07:00:00.000Z"
+    },
+    {
+      id: 3,
+      first_name: "Bob",
+      last_name: "Doe",
+      email: "bdoe@devevlopmenttesting.com",
+      password: "notRealForDev3!",
+      nickname: null,
+      home_state: "WY",
+      date_created: "2020-12-21T07:00:00.000Z"
+    },
+    {
+      id: 4,
+      first_name: "Steve",
+      last_name: "Doe",
+      email: "sdoe@devevlopmenttesting.com",
+      password: "notRealForDev4!",
+      nickname: "parks_lover",
+      home_state: null,
+      date_created: "2020-12-21T07:00:00.000Z"
+    }
+  ];
+}
