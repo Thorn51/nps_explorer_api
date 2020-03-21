532fab1eeb765a40be4c9e2ad815670c4723883f
Matt Thornberry <mtberry16@hotmail.com>
2020-03-20 21:33:15 -0600

    Add validation of the JWT

---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,30 @@
+const AuthService = require("../auth/auth-service");
+
+// Validate the JWT
+function requireAuth(req, res, next) {
+  const authToken = req.get("Authorization") || "";
+  let bearerToken;
+  if (!authToken.toLowerCase().startWith("bearer ")) {
+    return res.status(401).json({ error: "Missing bearer token" });
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
+  }
+  try {
+    const payload = AuthService.verifyJwt(bearerToken);
+
+    AuthService.getUserWithUsername(req.app.get("db"), payload.sub)
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: "Unauthorized request" });
+        }
+        req.user = user;
+        next();
+      })
+      .catch(err => {
+        console.log(err);
+        next(err);
+      });
+  } catch (error) {
+    res.status(401).json({ error: "Unauthorized request" });
+  }
+}
