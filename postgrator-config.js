41189ba30252fb35cdbf64adceaaf20df65cfaa3
Matt Thornberry <mtberry16@hotmail.com>
2020-03-21 09:20:50 -0600

    Fix connection string error

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,6 +6,6 @@ module.exports = {
   connectionString:
     process.env.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
-      : process.eventNames.DATABASE_URL,
+      : process.env.DATABASE_URL,
   SSS: !!process.env.SSL
 };
