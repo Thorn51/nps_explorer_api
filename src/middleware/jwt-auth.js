5d64df26f686e3f088f00559f9599ce49c38da27
Matt Thornberry <mtberry16@hotmail.com>
2020-03-22 20:29:19 -0600

    Fix error

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -4,7 +4,7 @@ const AuthService = require("../auth/auth-service");
 function requireAuth(req, res, next) {
   const authToken = req.get("Authorization") || "";
   let bearerToken;
-  if (!authToken.toLowerCase().startWith("bearer ")) {
+  if (!authToken.toLowerCase().startsWith("bearer ")) {
     return res.status(401).json({ error: "Missing bearer token" });
   } else {
     bearerToken = authToken.slice(7, authToken.length);
