dade95096b5e3023376815ee69ec38a77aa71a96
Matt Thornberry <mtberry16@hotmail.com>
2020-03-24 19:00:25 -0600

    Prepping for deployement

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use("/api/favorites", favoriteParksRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
     console.log(error);
